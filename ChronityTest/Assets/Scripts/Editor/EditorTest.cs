89ec462e258b9b394272519adcb75399f9b6a98f
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-09-11 20:34:55 +0200

    update test project

---
+++ ChronityTest/Assets/Scripts/Editor/EditorTest.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Chronity.Editor;
+
+public class EditorTest : EditorWindow
+{
+    private EditorTimer timer;
+
+    [MenuItem("Window/Chronity")]
+    public static void ShowWindow()
+    {
+        GetWindow<EditorTest>("Chronity Test");
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Chronity Test"))
+        {
+            timer = EditorTimer.Register(5, () => Debug.Log("Hello World"), x => Debug.Log(x));
+        }
+        if (GUILayout.Button("Cancel"))
+        {
+            timer.Cancel();
+        }
+        if (GUILayout.Button("Pause"))
+        {
+            timer.Pause();
+        }
+        if(GUILayout.Button("Resume"))
+        {
+            timer.Resume();
+        }
+    }
+}
